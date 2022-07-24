0a35e6f23730a84b560a3fa16d0658b284cdd3f1
Ultra-max99 <80412399+Ultra-max99@users.noreply.github.com>
2022-07-23 18:29:41 -0700

    Added Main PKG

---
+++ package/QuestionWord/pronouns.js
@@ -0,0 +1,13 @@
+const pronouns = [
+  ["she", "her", "her", "hers", "herself"],
+  ["he", "him", "his", "his", "himself"],
+  ["they", "them", "their", "theirs", "themself"],
+  ["they", "them", "their", "theirs", "themselves"],
+  ["i", "me", "my", "mine", "myself"],
+  ["you", "you", "your", "yours", "yourself"],
+  ["you", "you", "your", "yours", "yourselves"],
+  ["it", "it", "its", "its", "itself"],
+  ["we", "us", "our", "ours", "ourselves"]
+];
+
+export default pronouns;
